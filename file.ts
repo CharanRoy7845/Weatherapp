391f491edcff02a7cf72bdbdac4fb33af47a44c6
Sai Charan Sapineni <115992736+CharanRoy7845@users.noreply.github.com>
2024-01-03 21:18:14 +0530

    Update file.ts

--- file.ts
+++ file.ts
@@ -1,1 +1,31 @@
+// weather.component.ts
+import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent {
+  city: string = '';
+  weatherData: any;
+  error: string = '';
+
+  constructor(private weatherService: WeatherService) {}
+
+  getWeather() {
+    this.weatherService.getWeather(this.city)
+      .subscribe(
+        (data) => {
+          this.weatherData = data;
+          this.error = '';
+        },
+        (error) => {
+          this.error = 'City not found';
+          this.weatherData = null;
+        }
+      );
+  }
+}
 
